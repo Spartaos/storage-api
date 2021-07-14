52145a99762244779937428b88cf873fd7059a9a
Spartaos <metreos@hotmail.com>
2021-07-13 17:18:40 -0700

    Crecion de questpo.py

---
+++ routes/questpo.py
@@ -0,0 +1,43 @@
+from json import dumps as json_dumps
+from os import environ
+import bottle
+from modules.cors import enable_cors
+import modules.utils as utils
+from modules.auth import auth_required
+
+app = bottle.Bottle()
+
+## Add a user
+@app.post("/quest_po/addUser")
+def add_a_user(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
+
+## Get user info
+@app.get("/quest_po/addUser/<name_id>")
+def get_user(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
+
+## agregar pregunta
+@app.post("POST /quest-po/addquestion")
+def add_a_quest(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
+
+## Get videogames List
+@app.get("/quest-po/list")
+def get_quest_list(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
+
+## Get the info for a videogame
+@app.get("/quest-po/<respuesta_id>")
+def get_a_videogame(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
