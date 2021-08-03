61579e52a196b3e7c32d206e45544f492f9e0d11
Spartaos <metreos@hotmail.com>
2021-08-03 16:53:08 -0700

    Agregado de modulos

--- modules/quest_po.py
+++ modules/quest_po.py
@@ -6,27 +6,26 @@ import jwt
 import binascii
 from functools import wraps
 import models.auth as mauth
+import json
+from modules.storage import *
 
-questions = []
-def add_a_quest(clave_pre, clave_usuario, datetime, clave_tema):
-    Questions = {
-        "clave_pre": clave_pre,
-        "Usuario": clave_usuario,
-        "Datetime": datetime,
-        "Tema": tema,
+def add_a_quest(clave_pre=None, clave_usuario=None, pregunta=None, datetime=None, clave_tema=None):
+    print("almacenado en add_a_quest")
+    print(clave_pre, pregunta,clave_tema)
+    datos_almacenados = {"clave_pre": clave_pre,"pregunta": pregunta, "datetime": datetime, "clave_tema":clave_tema }
+    nombr_de_archivo = f"{clave_pre}-{pregunta}.json"
+    datos = store_string(
+        "Carpeta1",
+        nombr_de_archivo,
+        json.dumps(datos_almacenados)
+    )
+    return "Exito"
 
-    }
-    questions.append(questions)
-    return json.dumps(questions)
 
-respuesta = []
-def add_a_respuesta(clave_pre, clave_usuario, datetime, clave_tema):
-    Respuesta = {
-        "clave_pre": clave_pre,
-        "Usuario": clave_usuario,
-        "Datetime": datetime,
-        "Tema": tema,
-
-    }
-    respuesta.append(respuesta)
-    return json.dumps(respuesta)
+def reg_respuesta(clave_pre=None, clave_usuario=None, datetime=None,respuesta=None clave_tema=None):
+    print("Guardar en reg_respuesta")
+    print(respuesta)
+    para_almacenar = {"clave_usuario": clave_usuario,"respuesta": respuesta, "datetime": datetime, "clave_tema":clave_tema }
+    json_text = json.dumps(datos_almacenados)
+    store_string('rescarpeta', respuesta, datos_almacenados)
+    return "Exito"
