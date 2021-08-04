c373cd72c06c8f4fa0b7fd4211be88d9dd475dad
Spartaos <metreos@hotmail.com>
2021-08-03 18:25:02 -0700

    funcion preguntas

--- routes/quest_po.py
+++ routes/quest_po.py
@@ -31,7 +31,7 @@ def get_user(*args, **kwargs):
 
 ## agregar pregunta
 @app.post("/addquest")
-def AddVg(*args, **kwargs):
+def addquest(*args, **kwargs):
 
     payload = bottle.request.json
     print(payload)
