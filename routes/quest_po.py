49bcea64b34973ff20ec3ca997b736216f79ddb4
Spartaos <metreos@hotmail.com>
2021-08-07 15:04:58 -0700

    Lsita

--- routes/quest_po.py
+++ routes/quest_po.py
@@ -15,6 +15,8 @@ app = BottleJson()
 @app.post("/")
 @app.get("/")
 ## agregar pregunta
+##$ curl http://localhost:8080/quest_po/addquest -X POST -H 'content-Type: application/json' -d '{"clave_pre":"212","clave_Usuario": "unis","pregunta": "Que son las TICS?", "clave_tema" : "TICS"}'
+
 
 @app.post("/addquest")
 def addquest(*args, **kwargs):
@@ -38,7 +40,15 @@ def addquest(*args, **kwargs):
         raise bottle.HTTPError(400)
     raise bottle.HTTPError(501)
 
-
+##ver preguntas
+#curl http://localhost:8080/vg_info/list -X GET
+@app.get("/list")
+def get_quest(*args, **kwargs):
+    try:
+       respuesta = add_a_quest()
+    except:
+        raise bottle.HTTPError(500, "Error interno")
+    raise bottle.HTTPError(200, respuesta)
 ## Add a user
 @app.post("/addUser")
 def add_a_user(*args, **kwargs):
@@ -53,50 +63,6 @@ def get_user(*args, **kwargs):
     bottle.response.status = 501
     bottle.response.content_type = "application/json"
     return dict(code=501, message="Not implemented")
-
-## agregar pregunta
-@app.post("/addquest")
-def addquest(*args, **kwargs):
-
-    payload = bottle.request.json
-    print(payload)
-    try:
-        clave_pre = str(payload['clave_pre'])
-        clave_usuario = str(payload['clave_usuario'])
-        pregunta = str(payload['pregunta'])
-        datetime = str(payload['datetime'])
-        clave_tema = str(payload['clave_tema'])
-        if len(clave_usuario) == 0:
-            raise Exception()
-        # Validacion de fecha
-        year, month, date = [int(x) for x in
-        datetime.split("-")]
-        print("Datos validos")
-    except:
-        print("Datos invalidos")
-        raise bottle.HTTPError(400)
-    raise bottle.HTTPError(501)
-
-## agregar respuesta
-@app.post("/addquest")
-def addquest(*args, **kwargs):
-
-    payload = bottle.request.json
-    print(payload)
-    try:
-        clave_pre = str(payload['clave_pre'])
-        clave_usuario = str(payload['clave_usuario'])
-        repuesta = str(payload['pregunta'])
-        clave_tema = str(payload['clave_tema'])
-        if len(clave_usuario) == 0:
-            raise Exception()
-        print("Datos validos")
-        respuesta = reg_respuesta(**payload)
-        raise HTTPError(201, respuesta)
-    except:
-        print("Datos invalidos")
-        raise bottle.HTTPError(400)
-    raise bottle.HTTPError(500)
 
 
 @app.post("POST /quest-po/addquestion")
