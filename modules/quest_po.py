9031be1c57213fbad76683527ef03317042ebddc
Spartaos <metreos@hotmail.com>
2021-08-09 14:45:05 -0700

    Actualiza

--- modules/quest_po.py
+++ modules/quest_po.py
@@ -1,14 +1,5 @@
-from os import environ, urandom
-import hashlib
-import datetime
-from bottle import response, request
-import jwt
-import binascii
-from functools import wraps
-import models.auth as mauth
-
 import json
-
+from datetime import datetime
 from modules.storage import (
     store_string,
     store_bytes,
@@ -16,36 +7,98 @@ from modules.storage import (
     get_storage_file
 )
 
-def add_a_quest(clave_pre=None, clave_usuario=None, pregunta=None, datetime=None, clave_tema=None):
-    print("almacenado en add_a_quest")
-    print(clave_pre, pregunta, clave_tema)
-    datos_almacenados = {"clave_pre": clave_pre,"pregunta": pregunta, "datetime": datetime, "clave_tema":clave_tema }
-    nombr_de_archivo = f"{clave_pre}-{pregunta}.json"
+def add_user(id = None, username = None, genero = None, edad = None,  fecha = None, correo = None):
+
+    print("Datos de usario")
+    print(id, username, genero, edad, fecha, correo)
+    print("Capturdo")
+
+
+    almacenable = {"id": id, "username": username, "genero": genero, "edad": edad, "fecha": fecha, "correo": correo,}
+    nombre_de_archivo = f"{username}-{id}-{genero}-{edad}-{fecha}-{correo}.json"
     datos = store_string(
-        "quest_po/Carpeta1",
-        nombr_de_archivo,
+        "user/users",
+        nombre_de_archivo,
+        json.dumps(almacenable)
+    )
+    return datos
+
+def get_user_list(users=None):
+    query_result = query_storage(
+        "user/users",
+    )
+    if users is None:
+        return query_result["content"]
+
+def get_id_details(id=None):
+    query_result = query_storage(
+        "user/users",
+    )
+    if id is not None:
+        return [
+           r
+           for r in query_result["content"]
+           if id in r
+        ]
+    print("done")
+
+
+def add_quest(clave_pre=None, clave_usuario=None, pregunta=None, datetime=None, clave_tema=None):
+    print("Datos de Pregunta")
+    print(clave_pre, clave_usuario pregunta, clave_tema)
+    print("Exito")
+
+    datos_almacenados = {"clave_pre": clave_pre,"pregunta": pregunta, "datetime": datetime, "clave_tema":clave_tema }
+    nombre_de_archivo = f"{pregunta}-{clave_pre}-{clave_usuario}.json"
+    datos = store_string(
+        "pregunta/preguntas",
+        nombre_de_archivo,
         json.dumps(datos_almacenados)
     )
-    return "Exito"
+    return datos
 
-def get_user_list(pregunta=None):
+def get_preguntas( clave_pre= None, pregunta = None):
     query_result = query_storage(
         "pregunta/preguntas",
     )
-    if preguntas is None:
-        return query_result["content"]
+    if id is not None:
+        return [
+           r
+           for r in query_result["content"]
+           if id in r
+        ]
+        print("Respuesta")
+    if encuesta is not None:
+        return [
+           r
+           for r in query_result["content"]
+           if encuesta in r
+        ]
+        print("respuesta")
 
-def reg_respuesta(clave_pre=None, clave_usuario=None, datetime=None,respuesta=None, clave_tema=None):
-    print("Guardar en reg_respuesta")
+
+def add_answer(clave_pre=None, clave_usuario=None, datetime=None,respuesta=None, clave_tema=None):
+    print("Respuesta guardada")
     print(respuesta)
-    para_almacenar = {"clave_usuario": clave_usuario,"respuesta": respuesta, "datetime": datetime, "clave_tema":clave_tema }
-    json_text = json.dumps(datos_almacenados)
-    store_string('rescarpeta', respuesta, datos_almacenados)
-    return "Exito"
+    print("Exito")
 
-def get_quest_list(clave_pre=None):
-    query_result = query_storage(
-        "quest_po/Carpeta",
+    datos_almacenados = {"clave_usuario": clave_usuario,"respuesta": respuesta, "datetime": datetime, "clave_tema":clave_tema }
+    nombre_de_archivo = f"{respuesta}-{clave_usuario}.json"
+    datos = store_string(
+        "pregunta/respuestas",
+        nombre_de_archivo,
+        json.dumps(datos_almacenados)
     )
-    if clave_pre is  None:
-        return query_result["content"]
+    return datos
+
+def get_answers(clave_pre=None, clave_usuario=None, datetime=None,respuesta=None, clave_tema=None):
+    query_result = query_storage(
+        "pregunta/respuestas",
+    )
+    if id is not None:
+        return [
+           r
+           for r in query_result["content"]
+           if id in r
+        ]
+    print("Exito")
